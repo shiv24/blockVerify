fcd51f24ada13fd4567cfa1209756bb45b1338ea
Gupta, Shiv (CA - Toronto) <shivxgupta@deloitte.ca>
2018-11-24 19:46:54 -0500

    .sol and 2_deploy

---
+++ private/startnode.sh
@@ -0,0 +1,1 @@
+geth --networkid 4224 --mine --minerthreads 1 --datadir "." --nodiscover --rpc --rpcport "8545" --port "30303" --rpccorsdomain "*" --nat "any" --rpcapi eth,web3,personal,net --unlock 0 --password ./password.sec --ipcpath "~/Library/Ethereum/geth.ipc"
